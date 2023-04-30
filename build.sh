65fed6b1ad7855a9c1867e14847632b533f319a2
chandrahmuki <amouyaljerome@gmail.com>
2023-04-30 09:43:55 +0200

    Update build.sh
    trying to install go here

--- build.sh
+++ build.sh
@@ -20,17 +20,4 @@ done
 echo "---"
 
 rpm-ostree override remove mesa-va-drivers-freeworld
-
-# install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
-pip install --prefix=/usr yafti
-
-# add a package group for yafti using the packages defined in recipe.yml
-flatpaks=$(yq '.flatpaks[]' < /tmp/ublue-recipe.yml)
-# only try to create package group if some flatpaks are defined
-if [[ -n "$flatpaks" ]]; then            
-    yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /usr/etc/yafti.yml
-    yq -i '.screens.applications.values.groups.Custom.default = true' /usr/etc/yafti.yml
-    for pkg in $(echo -e "$flatpaks"); do \
-        yq -i ".screens.applications.values.groups.Custom.packages += [{\"$pkg\": \"$pkg\"}]" /usr/etc/yafti.yml
-    done
-fi
+rpm-ostree install go
